40cb1021f02596209d0bf420ff98478ca9fb451c
RikhardGaming <146456703+RikhardGaming@users.noreply.github.com>
2024-01-08 14:18:10 +0100

    point count increase and decrease

---
+++ Assets/Script/PointSystem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSystem : MonoBehaviour
+{
+    [SerializeField] private int TotalPoints;
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Good"))
+        {
+            TotalPoints++;
+        }
+        if(other.tag == "Bad")
+        {
+            TotalPoints--;
+        }
+        if(other.tag == "Neutral")
+        {
+            Debug.Log("Neutral");
+
+        }
+    }
+}
