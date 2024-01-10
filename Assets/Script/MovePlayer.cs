ce9247e23a662b27f3c499bc3da9d6f1d7360985
RikhardGaming <146456703+RikhardGaming@users.noreply.github.com>
2024-01-10 11:58:48 +0100

    move player attempt

--- Assets/Script/MovePlayer.cs
+++ Assets/Script/MovePlayer.cs
@@ -4,8 +4,39 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+    private float minimum = 0f;
+    private float maximum = 1f;
+
+    static float t = 0f;
+
+
+
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject Player;
+    [SerializeField] private Transform RightTarget;
+    [SerializeField] private Transform RightTarget2;
+    [SerializeField] private Transform LeftTarget;
+    [SerializeField] private Transform LeftTarget2;
+
     private void Update()
     {
-       // MovePlayer.
+        
+
+        transform.position = new Vector3(Mathf.Lerp(minimum, maximum, speed), 0, 0);
+        speed += 0.5f * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, RightTarget.position, speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+        }
+
+
+
     }
+
 }
